865b79126861fa1e458483c2dbb7612289aadb47
K009 <spidahas@gmail.com>
2025-09-11 10:00:59 +0200

    Renamed dist to js, fixed package.json main and types fields.

---
+++ js/customEvents.d.ts
@@ -0,0 +1,142 @@
+import Highcharts from "highcharts";
+
+// Plugin function
+declare function HC_customEvents(H: typeof Highcharts): void;
+export default HC_customEvents;
+
+// ---- Highcharts augmentations ----
+declare module "highcharts" {
+  export let customEventsPluginLoaded: boolean;
+
+  interface Options {
+    events?: ElementEvents;
+  }
+
+  interface Axis {
+    axisTitle: Highcharts.SVGElement;
+    labelGroup: Highcharts.SVGElement;
+    plotLinesAndBands?: PlotLineOrBand[];
+    cross?: Highcharts.SVGElement;
+    stacking?: {
+      stackTotalGroup?: Highcharts.SVGElement;
+    };
+    stackLabels?: {
+      events?: ElementEvents;
+    };
+  }
+
+  interface Series {
+    group: Highcharts.SVGElement;
+    dataLabelsGroup: Highcharts.SVGElement;
+  }
+
+  interface Point {
+    dataLabel: Highcharts.SVGLabel;
+  }
+
+  interface Chart {
+    chartBackground: Highcharts.SVGElement;
+    _customEventsBound?: BoundEvent[];
+  }
+
+  interface SVGElement {
+    _eventBound?: Record<string, boolean>;
+  }
+
+  export interface SeriesPointOptions {
+    events?: PointEventsOptions | ElementEvents;
+  }
+
+  interface SeriesOptions {
+    dataLabels?: Highcharts.DataLabelsOptionsObject | Highcharts.DataLabelsOptionsObject[];
+    point?: SeriesPointOptions;
+  }
+
+  export interface ElementEvents {
+    click?: (e: Event | PointerEvent) => void;
+    dblclick?: (e: Event | PointerEvent) => void;
+    contextmenu?: (e: Event | PointerEvent) => void;
+    mouseover?: (e: Event | PointerEvent) => void;
+    mouseout?: (e: Event | PointerEvent) => void;
+    mousedown?: (e: Event | PointerEvent) => void;
+    mousemove?: (e: Event | PointerEvent) => void;
+  }
+
+  export interface BoundEvent {
+    element: Highcharts.SVGElement;
+    eventName: string;
+    handler: (e: Event | PointerEvent) => void;
+  }
+
+  export interface PlotLineOrBand {
+    label?: Highcharts.SVGElement;
+    options?: {
+      label?: {
+        events?: ElementEvents;
+      };
+    };
+  }
+
+  export interface PlotOptions {
+    label?: {
+      events?: ElementEvents;
+    };
+  }
+
+  export interface ZAxisOptions {
+    crosshair?: Highcharts.AxisCrosshairOptions;
+  }
+
+  export interface AxisCrosshairOptions {
+    events?: ElementEvents;
+  }
+
+  export interface YAxisStackLabelsOptions {
+    events?: ElementEvents;
+  }
+
+  interface XAxisPlotBandsLabelOptions {
+    events?: ElementEvents;
+  }
+  interface YAxisPlotBandsLabelOptions {
+    events?: ElementEvents;
+  }
+  interface ZAxisPlotBandsLabelOptions {
+    events?: ElementEvents;
+  }
+  interface XAxisPlotLinesLabelOptions {
+    events?: ElementEvents;
+  }
+  interface YAxisPlotLinesLabelOptions {
+    events?: ElementEvents;
+  }
+  interface ZAxisPlotLinesLabelOptions {
+    events?: ElementEvents;
+  }
+
+  export interface TitleOptions {
+    events?: ElementEvents;
+  }
+  export interface SubtitleOptions {
+    events?: ElementEvents;
+  }
+  export interface XAxisTitleOptions {
+    events?: ElementEvents;
+  }
+  export interface YAxisTitleOptions {
+    events?: ElementEvents;
+  }
+  export interface ZAxisTitleOptions {
+    events?: ElementEvents;
+  }
+
+  export interface XAxisLabelsOptions {
+    events?: ElementEvents;
+  }
+  export interface YAxisLabelsOptions {
+    events?: ElementEvents;
+  }
+  export interface ZAxisLabelsOptions {
+    events?: ElementEvents;
+  }
+}
